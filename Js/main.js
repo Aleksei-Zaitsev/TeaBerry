d0ad2be7905d7eacd6303d2abbf85dc9abdeb06f
Aleksei Zaitsev <Aleksei.Zaitsev.V@yandex.ru>
2020-07-24 02:24:55 +0300

    forth-commit

--- Js/main.js
+++ Js/main.js
@@ -167,8 +167,8 @@ $( document ).ready(function() {
         selectList.slideUp(0);
         selectHead.on('click', function() {
             if ( !$(this).hasClass('on') ) {
-                $('.new-select').css('border-bottom-left-radius', '0');
-                $('.new-select').css('border-bottom-right-radius', '0');
+                $('.select-selected').css('border-bottom-left-radius', '0');
+                $('.select-selected').css('border-bottom-right-radius', '0');
                 $(this).addClass('on');
                 selectList.slideDown(duration);
 
@@ -261,14 +261,14 @@ $( document ).ready(function() {
         }
         if (!select.val()) {
             select.siblings('.error-type').css('display', 'block');
-            select.css('borderColor', 'rgba(187,22,47)')
-            select.css('boxShadow', '0 0 3px 3px rgb(187,22,47,0.45)')
+            $('.select-selected').css('borderColor', 'rgba(187,22,47)')
+            $('.select-selected').css('boxShadow', '0 0 3px 3px rgb(187,22,47,0.45)')
             hasError = true;
         }
 
         if (select.val()) {
-            select.css('borderColor', 'rgb(22,187,30)')
-            select.css('boxShadow', '0 0 3px 3px rgb(22,187,30,0.45)')
+            $('.select-selected').css('borderColor', 'rgb(22,187,30)')
+            $('.select-selected').css('boxShadow', '0 0 3px 3px rgb(22,187,30,0.45)')
         }
 
         if (hasError) {
@@ -364,5 +364,96 @@ $( document ).ready(function() {
         }, 2000);
     });
 
-    //Убираем анимацию при адаптиве
+    //Select new version
+
+    var x, i, j, l, ll, selElmnt, a, b, c;
+    /* Look for any elements with the class "custom-select": */
+    x = document.getElementsByClassName("custom-select");
+    l = x.length;
+    for (i = 0; i < l; i++) {
+        selElmnt = x[i].getElementsByTagName("select")[0];
+        ll = selElmnt.length;
+        /* For each element, create a new DIV that will act as the selected item: */
+        a = document.createElement("DIV");
+        a.setAttribute("class", "select-selected");
+        a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+        x[i].appendChild(a);
+        /* For each element, create a new DIV that will contain the option list: */
+        b = document.createElement("DIV");
+        b.setAttribute("class", "select-items select-hide");
+        for (j = 1; j < ll; j++) {
+            /* For each option in the original select element,
+            create a new DIV that will act as an option item: */
+            c = document.createElement("DIV");
+            c.innerHTML = selElmnt.options[j].innerHTML;
+            c.addEventListener("click", function(e) {
+
+                /* When an item is clicked, update the original select box,
+                and the selected item: */
+                var y, i, k, s, h, sl, yl;
+                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                sl = s.length;
+                h = this.parentNode.previousSibling;
+                for (i = 0; i < sl; i++) {
+                    if (s.options[i].innerHTML == this.innerHTML) {
+                        s.selectedIndex = i;
+                        h.innerHTML = this.innerHTML;
+                        y = this.parentNode.getElementsByClassName("same-as-selected");
+                        yl = y.length;
+                        for (k = 0; k < yl; k++) {
+                            y[k].removeAttribute("class");
+                        }
+                        this.setAttribute("class", "same-as-selected");
+                        break;
+                    }
+                }
+                h.click();
+                $('.select-selected').css('border-bottom-left-radius', '24px');
+                $('.select-selected').css('border-bottom-right-radius', '24px');
+            });
+            b.appendChild(c);
+        }
+        x[i].appendChild(b);
+        a.addEventListener("click", function(e) {
+            /* When the select box is clicked, close any other select boxes,
+            and open/close the current select box: */
+            e.stopPropagation();
+            closeAllSelect(this);
+            $('.select-selected').css('border-bottom-left-radius', '0px');
+            $('.select-selected').css('border-bottom-right-radius', '0px');
+            this.nextSibling.classList.toggle("select-hide");
+            this.classList.toggle("select-arrow-active");
+        });
+    }
+
+    function closeAllSelect(elmnt) {
+        $('.select-selected').css('border-bottom-left-radius', '24px');
+        $('.select-selected').css('border-bottom-right-radius',  '24px');
+        /* A function that will close all select boxes in the document,
+        except the current select box: */
+        var x, y, i, xl, yl, arrNo = [];
+
+        x = document.getElementsByClassName("select-items");
+        y = document.getElementsByClassName("select-selected");
+        xl = x.length;
+        yl = y.length;
+        for (i = 0; i < yl; i++) {
+            if (elmnt == y[i]) {
+                arrNo.push(i)
+
+            } else {
+
+                y[i].classList.remove("select-arrow-active");
+            }
+        }
+        for (i = 0; i < xl; i++) {
+            if (arrNo.indexOf(i)) {
+                x[i].classList.add("select-hide");
+            }
+        }
+    }
+
+    /* If the user clicks anywhere outside the select box,
+    then close all select boxes: */
+    document.addEventListener("click", closeAllSelect);
 });
